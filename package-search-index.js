6f3b5cdac97bce96c73b6f7cba09ad5e613bfa28
Christian <litkec.tin20@student.dhbw-heidenheim.de>
2022-02-07 11:51:12 +0100

    Webpage update through script

--- package-search-index.js
+++ package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"chat"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"zork"}];updateSearchResults();
